835542a08fe086342831e1b01766ebc2fee0404c
Dr. Waldijk <DokterW@users.noreply.github.com>
2017-06-19 11:47:58 +0200

    fix

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# fnugg v0.22
+# fnugg v0.23
 # Made by Dr. Waldijk
 # A simple weather script that fetches weather data from darksky.net.
 # Read the README.md for more info, but you will find more info here below.
 # By running this script you agree to the license terms.
 # Config ----------------------------------------------------------------------------
 FNUNAM="fnugg"
-FNUVER="0.22"
+FNUVER="0.23"
 FNUDIR="$HOME/.dokter/fnugg"
 # Your API key for darksky.net
 # FNUSKY=""
@@ -282,8 +282,8 @@ while :; do
                     clear
                     echo "$FNUNAM - v$FNUVER :: powered by darksky.net"
                     echo ""
-                    echo -n "      Location:"
-                    echo "               $FNULOC" | fmt -w $FNUCOL -c | sed -r '1s/^\s{10}/ /'
+                    echo -n "     Location:"
+                    echo "               $FNULOC" | fmt -w $FNUCOL -c | sed -r '1s/^\s{15}/ /'
                     echo ""
                     until [ "$FNUCANT" -eq "6" ]; do
                         FNUCANT=$(expr $FNUCANT + 1)
